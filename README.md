f3e5a3f95af073b7cf72259dd3bbdc3e4821e5c7
Ian Neal Higginson <iannhigginson@hotmail.com>
2022-11-16 21:02:02 +0200

    version 1.161122.21

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ I don't want a bulky library.
 This is light with a slightly different syntax.
 
 - @package jsegret.js
-- @version 1.0.03012022.2245
+- @version 1.161122.21
 - @author Ian Neal Higginson
 - @email iannhigginson@hotmail.com
-- @date 03/01/2022
+- @date 16/11/2022
 
- A very simple jQuery clone. I don't need the whole library.
+A very simple jQuery clone. I don't need the whole library.
--- js/jsegret.js
+++ js/jsegret.js
@@ -1,10 +1,10 @@
 /**
  *
  * @package jsegret.js
- * @version 1.0.03012022.2245
+ * @version 1.161122.21
  * @author Ian Neal Higginson
  * @email iannhigginson@hotmail.com
- * @date 03/01/2022
+ * @date 16/11/2022
  *
  ** A very simple jQuery clone. I don't need the whole library.
  *
@@ -12,22 +12,24 @@
 
 //#region _
 function _(param) {
- if (param.indexOf('#') === 0) {
+ if (param.indexOf("#") === 0) {
   /**
-   * This comes back as a single element (as in "id").
-   * An example of how to drive it:
-   *   _('#one').innerHTML = "Hello World.";
+   *? This comes back as a single element (as in "id").
+   *? An example of how to drive it:
+   **   _('#one').innerHTML = "Hello World.";
    */
   return document.getElementById(param.substr(1, param.length));
  }
- if (param.indexOf('.') === 0) {
+ if (param.indexOf(".") === 0) {
   /**
-   * This comes back as a collection of elements (as in "class").
-   * An example of how to drive it:
-   *   let one = _('.one');
-   *   for(let i = 0; i < one.length; i++){
-   *    one[i].setAttribute("style", "color: black;");
-   *   }
+   *
+   *? This comes back as a collection of elements (as in "class").
+   *? An example of how to drive it:
+   **   let one = _('.one');
+   **   for(let i = 0; i < one.length; i++){
+   **    one[i].setAttribute("style", "color: black;");
+   **   }
+   *
    */
   return document.querySelectorAll(param);
  }
@@ -36,13 +38,15 @@ function _(param) {
 
 //#region css
 /**
- *  _.css('.title',
-        `background-color: #c0c0c0;
-         border-radius: 5px;
-         color: #000;
-         padding: 5px;
-         text-align: center;
-         `);
+ *
+ **  _.css('.title',
+ **       `background-color: #c0c0c0;
+ **        border-radius: 5px;
+ **        color: #000;
+ **        padding: 5px;
+ **        text-align: center;
+ **        `);
+ *
  */
 _.css = (elem, style) => {
  let elements = document.querySelectorAll(elem);
@@ -54,11 +58,10 @@ _.css = (elem, style) => {
 
 //#region get
 /**
+ ** _.get("./json/a.json", data => {
+ **  console.log(data);
+ ** });
  *
-  _.get("./json/a.json", data => {
-   console.log(data);
-  });
-
  */
 _.get = (url, callback) => {
  var httpRequest = new XMLHttpRequest();
@@ -74,10 +77,10 @@ _.get = (url, callback) => {
 //#region post
 /**
  *
-  _.post('./php/a.php', JSON.stringify(dataObj), (response) => {
-   console.log({response});
-  });
-
+ ** _.post('./php/a.php', JSON.stringify(dataObj), (response) => {
+ **  console.log({response});
+ ** });
+ *
  */
 _.post = (url, request, callback) => {
  var httpRequest = new XMLHttpRequest();
@@ -91,19 +94,24 @@ _.post = (url, request, callback) => {
 //#endregion post
 
 //#region ready
+/**
+ *
+ ** _.ready(()=>{
+ **  console.log("Hello world!");
+ ** })
+ *
+ */
 _.ready = (...args) => {
- document.addEventListener('DOMContentLoaded', () => {
+ document.addEventListener("DOMContentLoaded", () => {
   execute = () => {
    var argsLen = args.length;
 
    for (i = 0; i < argsLen; i++) {
     args[i].call();
    }
-
   };
 
   execute();
-
  });
 };
 //#endregion ready
